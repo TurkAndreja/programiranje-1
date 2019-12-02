aa44b771cbcb6cb72db1198cae4ca90fd427dd23
Andreja Turk <andreja.turk@student.fmf.uni-lj.si>
2019-12-02 13:59:49 +0100

    programiranje 2. 12. 2019

--- 09-modularnost/vaje/moduli.ml
+++ 09-modularnost/vaje/moduli.ml
@@ -48,15 +48,22 @@
  pridobivamo naravno število iz celega števila.
 [*----------------------------------------------------------------------------*)
 
-module type NAT = sig
+module type NAT = sig (*ta tip t je ponavadi tak glavni tip modula in not bo mel vrednost zero, ki bo tipa t in neko funkcijo, ki bo sprejela dva argumenta tipa t in vrnila bool: povemo kako bo stvar delala*)
   type t
 
   val eq   : t -> t -> bool
   val zero : t
-  (* Dodajte manjkajoče! *)
-  (* val to_int : t -> int *)
-  (* val of_int : int -> t *)
-end
+  (* Dodajte manjkajoče! *) (*dodamo, da bo nekaj berljivo:pretvarjanje*)
+  val to_int : t -> int (*pri N dobimo to zastonj, le pazit mormo, da ni negativnih; implementiramo ih lahk tut z Peamovimi aksiomi*)
+  val of_int : int -> t 
+  val ena : t
+  val sestej : t -> t -> t
+  val odstej : t -> t -> t
+  val zmnozi : t -> t -> t 
+
+end (*delamo strukturo, ki se obnaša kot naravna števila*)
+(*ljudje bodo videli samo tole:glavne stvari, kar smo spodaj definirali pa ne: osnovno, kako se obnašajo naravna števila: kar pa se dejansko dogaja, lahko pa mal popravljaš, brez da bi vedeli*)
+
 
 (*----------------------------------------------------------------------------*]
  Napišite implementacijo modula [Nat_int], ki zgradi modul s signaturo [NAT],
@@ -64,14 +71,20 @@ end
 
  Namig: Dokler ne implementirate vse funkcij v [Nat_int] se bo OCaml pritoževal.
  Temu se lahko izognete tako, da funkcije, ki še niso napisane nadomestite z 
- [failwith "later"], vendar to ne deluje za konstante.
+ [failwith "later"], vendar to ne deluje za konstante. (*da ti scompila in pride čez. čeprav še nisi napisal*)
 [*----------------------------------------------------------------------------*)
 
 module Nat_int : NAT = struct
 
-  type t = int
-  let eq x y = failwith "later"
-  let zero = 0
+  type t = int (*tale eq že ve, da bo vzel dva tja in vrnil bool, ker to v def*)
+  let eq = (=) (*eq x y = x = y; tisto lažje, jo kar enačiš z (=) funkcijo-ne primeš direkt točke, ampak kar enačiš funkcije- to včasih hitreje*)
+  let zero = 0 
+  let ena = 1 (* tu smo vzeli 0 za nar št brez naslednika? ;zakaj rabimo potem let ena = 1*)
+  let sestej = (+)
+  let zmnozi = ( * ) (*pazi, moraš nujno imeti presledke, da ni komentar*)
+  let odstej x y = max 0 (x - y)
+  let to_int n = n
+  let of_int i = i 
   (* Dodajte manjkajoče! *)
 
 end
@@ -88,12 +101,43 @@ end
 
 [*----------------------------------------------------------------------------*)
 
-module Nat_peano : NAT = struct
+module Nat_peano : NAT = struct (*naravno število bi lahk tudi predstavljal s seznami, torej npr. z dolžinami seznamov*)
 
-  type t = unit (* To morate spremeniti! *)
-  let eq x y = failwith "later"
-  let zero = () (* To morate spremeniti! *)
+  type t = Zero | Succ of t 
+  let rec eq x y = 
+    match x, y with
+    | Zero, Zero -> true
+    (*| (Zero, Nas (n)) | (Nas (m), Zero) -> false (*pri ali dej v oklepaje obe strani*) *)
+    | Succ (n), Succ (m) -> eq n m
+    | _ -> false 
+
+  let zero = Zero (* To morate spremeniti! *)
   (* Dodajte manjkajoče! *)
+  let ena = Succ zero
+
+  let rec to_int n = match n with
+    | Zero -> 0
+    | Succ k -> 1 + (to_int k) (*tuki sestej ne dela, če ga dasm namest +*)
+
+  let rec of_int x = match x with
+    | 0 -> Zero
+    | x' when x' > 0 -> Succ (of_int (x' - 1))
+    | _ -> failwith "Negativnih števil ne sprejemam" (* lahk bi dal tle tut int -> t option in v tem primeru pač ne bi ničesar vrglo*)
+
+  let rec sestej m n =
+    match m with (*vseen, katerega matchamo*)
+    | Zero -> n
+    | Succ k -> Succ (sestej k n)
+     
+  let rec odstej m n = (*DN a - b = n+ - m+ = n - m in potem lovim ničlo*)
+    (*match m n with
+    | (Zero, _) | (_, Zero) -> Zero
+    | Succ (x), Succ (y) -> odstej *)
+
+  let rec zmnozi m n =
+    match m with
+    | Zero -> Zero
+    | Succ(x) -> sestej n (zmnozi n x)
 
 end
 
@@ -118,7 +162,17 @@ end
  - : int = 4950
 [*----------------------------------------------------------------------------*)
 
-let sum_nat_100 (module Nat : NAT) = ()
+let sum_nat_100 (module Nat : NAT) = (*lahk tut gremo z vsoto aritm. zap n(n+1)/2*)
+  let rec sum acc current_nat = 
+    if Nat.eq current_nat (Nat.of_int 100) then
+    acc else
+    sum (Nat.sestej current_nat (acc)) (Nat.add Nat.one current_nat)
+  Nat.to_int (sum Nat.zero Nat.zero)
+  (*linearna hitrost, ker se čez vse zapeljemo*)
+  (*važn e. če damo nekje skos veliko oz majhno stvar*)
+  (*linearna krat linearna je kvadratna potem*)
+  (* če bi nam nekdo povedal cifro in bi jo mogl še zgradit z nar št, že takoj dobimo kvadratno*)
+  
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Now we follow the fable told by John Reynolds in the introduction.
@@ -131,8 +185,16 @@ let sum_nat_100 (module Nat : NAT) = ()
 [*----------------------------------------------------------------------------*)
 
 module type COMPLEX = sig
-  type t
+  type t 
+
   val eq : t -> t -> bool
+  val zero = t
+  val ena = t
+  val i = t
+  val neg = t -> t
+  val conj = t -> t
+  val sestej = t -> t -> to_int
+  val zmnozi = t -> t -> t
   (* Dodajte manjkajoče! *)
 end
 
