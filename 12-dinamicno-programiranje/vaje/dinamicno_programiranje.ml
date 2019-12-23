75b1541f20daa8021fba86a1402ea90ebda27f73
Andreja Turk <andreja.turk@student.fmf.uni-lj.si>
2019-12-23 14:00:45 +0100

    programiranje 23. 12. 2019

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
@@ -21,6 +21,43 @@ let test_matrix =
      [| 2 ; 4 ; 5 |];
      [| 7 ; 0 ; 1 |] |]
 
+(* matriki okoli daj same ničle, da se ne sprašuješ vedno, če si že na robu*)
+
+
+(*let max_cheese matrix = function
+
+  let sirina = Array.length matrix.(0) in
+  let visina = Array.length matrix in
+
+  let aux x y = function
+    tren_vr = matrix.(x).(y)
+    desno = matrix.(x + 1).(y) if (x + 1) <=
+    spodaj = matrix.(x).(y + 1)
+*)
+  
+let max_cheese cheese = 
+  let height = Array.length cheese in
+  let width = Array.length cheese.(0) in
+  let memo = Array.make_matrix height width 0 in
+  let poracunaj_celico (vr:int) (st:int) = (*bo le spremenila, torej vrnila nič*)
+    if st = -1 then () else (
+    let desno = if st = width - 1 then 0 else memo.(vr).(st + 1) in 
+    let dol = if vr = height - 1 then 0 else memo.(vr + 1).(st) in 
+    memo.(vr).(st) <- cheese.(vr).(st) + (max desno dol);
+    poracunaj_celico vr (st - 1)
+    )
+  let rec vrsticno (vr:int) = (*za vrstico vr bo ustrezno popravila memo, nočemo pa da nam kr neki vrača, zato bo vrnila unit, ko bo nrdila vrstico vr, bo šla na vr - 1*)
+    if vr = -1 then ( (*rešimo se hitro, kdaj se konča*)
+    else (
+      (*let trenutna = cheese.(vr) in () (*lahk še funkcijo za po stolpcu dodam pred to po vrsticah, lahko pa jo tu dodam*)*)
+      poracunaj_celico vr (width - 1);
+      vrsticno(vr - 1)
+    ) 
+  in
+  vrsticno (height - 1);
+  memo.(0).(0) (*ker je array podan po vrsticah, je tu lažje po vrsticah iti*) (*v pythonu naredimo le dve gnezdeni zanki, na izpitu pri dim prog lahko delaš v poljubnem jeiku*)
+
+(*časovna zahtevnost je vsaj n krat m, ker gremo čez vse celice, iz vsake smo sam eno levo in eno desno pogledal in to je to, torej je skupak ravno n krat m*)
 (*----------------------------------------------------------------------------*]
  Rešujemo problem sestavljanja alternirajoče obarvanih stolpov. Imamo štiri
  različne tipe gradnikov, dva modra in dva rdeča. Modri gradniki so višin 2 in
@@ -37,6 +74,9 @@ let test_matrix =
  - : int = 35
 [*----------------------------------------------------------------------------*)
 
+let alternating_towers visina =
+
+let rec na_vrhu_modri =
 
 (*----------------------------------------------------------------------------*]
  Na nagradni igri ste zadeli kupon, ki vam omogoča, da v Mercatorju kupite
